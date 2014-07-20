9d58cdad8b2d4155469cba28d1ec6135b13f2073
James Boggs <jmsboggs@gmail.com>
2014-07-20 19:23:53 -0400

    Added external config to allow for use with multiple accounts
    Signed-off-by: James Boggs <jmsboggs@gmail.com>

--- RecruitmentBot.py
+++ RecruitmentBot.py
@@ -1,12 +1,12 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
+# Name:        Alert Module
+# Purpose:      Allows Prime to send out pre-battle alert messages
 #
 # Author:      James Boggs
 #
 # Created:     11/03/2014
 # Copyright:   (c) James Boggs 2014
-# Licence:     <your licence>
+# Licence:     GNU shit
 #-------------------------------------------------------------------------------
 
 #IMPORTS:#
@@ -14,21 +14,27 @@ import praw
 import string
 from pprint import pprint
 from time import sleep
+config = open('prime_cnfg.txt')
+config = config.read()
+config = config.split(',')
+userID = config[0]
+userPass = config[1]
 log = open("RecruitmentRunLog.txt","w")
 log.write("Finished Imports\n")
+from requests.exceptions import HTTPError
 #END IMPORTS#
+
 
 #REDDIT LOGIN
 r = praw.Reddit('Periwinkle Prime Recruitment')
 tries = 0
 while tries<11:
     try:
-        r.login('Periwinkle_Prime','periwinklerules')
+        r.login(userID,userPass)
         log.write("Logged In\n")
         break
     except:
         log.write("Log in error, trying again\n")
-
 
 def getUsers():
     signupThread = r.get_submission(submission_id='206qef')
@@ -41,18 +47,22 @@ def getUsers():
     for signUp in signUps:
         if detect_ORed(signUp.author):
             print("Orangered "+str(signUp.author)+" ignored!")
+            log.write("Orangered "+str(signUp.author)+" ignored!")
             continue
         recruit = signUp.author.__str__()
         log.write(str(signUp)+"\n")
         try:
             if not (recruit in troopList):
                 troopList.append(recruit)
+                log.write("Added user "+recruit+"to troopList.\n")
+                log.flush()
         except:
             log.write("ERROR:"+"\n")
             log.write(str(recruit)+"\n")
             pass
     log.write("Retrieved Majors"+"\n")
     log.write(str(troopList)+"\n")
+    print troopList
     return troopList
 
 def checkForGo(troopList):
@@ -64,6 +74,7 @@ def checkForGo(troopList):
                 PM.mark_as_read()
                 sLine = PM.subject.strip().upper()
                 if (sLine == "SEND MESSAGE") and (PM.author.__str__() in ['RockdaleRooster','Tiercel','Sahdee']):
+                    sent_to = ''
                     for troops in troopList:
                         try:
                             r.send_message(troops,"Battle Reminder",PM.body)
@@ -77,13 +88,17 @@ def checkForGo(troopList):
         sleep(60)
 
 def detect_ORed(user):
-    for subName in ['orangered','Oraistedearg','Aegis_Imperial','AreusAntris','GreatAurantiaco','NovumPersarum','MetropolisDaja','OrangeLondo','Pasto_Range','Tentorahogo']:
-        subReddit = r.get_subreddit(subName)
-        mods = subReddit.get_moderators()
-        for mod in mods:
+    subReddit = r.get_subreddit('orangered')
+    mods = subReddit.get_moderators()
+    for mod in mods:
+        try:
             if mod == user:
                 return True
-        return False
+        except HTTPError:
+            print ("User no longer exists?")
+            log.write("HTTPError again, with user "+str(user)+"\n")
+            continue
+    return False
 
-#troopList = getUsers()
-checkForGo(['Eliminioa'])
+troopList = getUsers()
+checkForGo(troopList)
